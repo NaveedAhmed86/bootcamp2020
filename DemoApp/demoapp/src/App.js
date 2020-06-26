f5283174ce6e605034626b9ffac5a16664dd74b6
Naveed Ahmed <navedsoomro@gmail.com>
2020-06-26 16:37:40 +0500

    Initialize prject usind React App

---
+++ DemoApp/demoapp/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import App from './App.css';
+import Colours from './Colours.js';
+
+function App() {
+
+  return (
+
+    <div className='App'>
+
+    <Colours />
+
+  </div>
+
+  );
+}
+
+export default App;
---
+++ DemoApp/demoapp/src/Colours.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+fuction Colours() {
+
+    return (
+   
+       <h1>Rainbow has seven Shades</h1>
+
+    );
+}
+
+export default Colours;
